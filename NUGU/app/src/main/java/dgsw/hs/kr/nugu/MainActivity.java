7df1abf65565e822ca139c3d86ab73b856b3a1da
Yoominseo <min123seo@naver.com>
2017-07-24 14:04:12 +0900

    project 생성2

---
+++ NUGU/app/src/main/java/dgsw/hs/kr/nugu/MainActivity.java
@@ -0,0 +1,13 @@
+package dgsw.hs.kr.nugu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
