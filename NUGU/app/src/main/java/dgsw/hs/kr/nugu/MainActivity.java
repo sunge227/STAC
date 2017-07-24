906457f2e03fb7cdcdea8420e6a130fc328e531f
minseong <damul0202@naver.com>
2017-07-24 14:49:50 +0900

    image
    이미지 넣고 클릭 이벤트 만듬
    onclick 안에 음성인식 시작 함수 넣으면 됨

--- NUGU/app/src/main/java/dgsw/hs/kr/nugu/MainActivity.java
+++ NUGU/app/src/main/java/dgsw/hs/kr/nugu/MainActivity.java
@@ -2,6 +2,9 @@ package dgsw.hs.kr.nugu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
+
+        ImageView mouse = (ImageView)findViewById(R.id.Mouse);
+
+        mouse.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                if(v.getId() ==R.id.Mouse){
+                    Log.d("tag","test");
+                }
+            }
+        });
     }
 }
