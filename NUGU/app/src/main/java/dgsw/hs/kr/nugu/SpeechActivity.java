2655ad6f4a9a31fb465a6985632e0d14008f0d16
Yoominseo <min123seo@naver.com>
2017-07-24 14:54:53 +0900

    speech_activity 추가

---
+++ NUGU/app/src/main/java/dgsw/hs/kr/nugu/SpeechActivity.java
@@ -0,0 +1,85 @@
+package dgsw.hs.kr.nugu;
+
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeechActivity extends AppCompatActivity {
+
+    Intent i;
+    SpeechRecognizer mRecognizer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech);
+
+        i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(listener);
+        mRecognizer.startListening(i);
+    }
+    private RecognitionListener listener = new RecognitionListener() {
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onError(int error) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            // TODO Auto-generated method stub
+
+        }
+    };
+}
